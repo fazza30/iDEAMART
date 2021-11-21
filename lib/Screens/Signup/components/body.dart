be7384b319ab3eca2aed60338dc199cfff5cb28f
Fazza Mufti Taufiqi <fazza.mufti@gmail.com>
2021-11-21 22:56:31 +0700

    Projects Started

---
+++ lib/Screens/Signup/components/body.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_auth/Screens/Signup/components/background.dart';
+import 'package:flutter_auth/components/already_have_an_account_acheck.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+import 'package:flutter_auth/components/rounded_input_field.dart';
+import 'package:flutter_auth/components/rounded_name_field.dart';
+import 'package:flutter_auth/components/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "BUAT AKUN ANDA SEKARANG",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            RoundedNameField(
+              hintText: "Masukkan Nama Anda",
+              onChanged: (value) {},
+            ),
+            RoundedInputField(
+              hintText: "Masukkan Nomor Telepon Anda",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/already_have_an_account_acheck.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/constants.dart';
+
+class AlreadyHaveAnAccountCheck extends StatelessWidget {
+  final bool login;
+  final Function press;
+  const AlreadyHaveAnAccountCheck({
+    Key key,
+    this.login = true,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          login ? "Belum memiliki akun ? " : "Sudah memiliki akun ? ",
+          style: TextStyle(color: kPrimaryColor),
+        ),
+        GestureDetector(
+          onTap: press,
+          child: Text(
+            login ? "Daftar" : "Masuk",
+            style: TextStyle(
+              color: kPrimaryColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
