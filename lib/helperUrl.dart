1d46cd531da54cba03e267810de6b72bf232d0bc
ranks72 <ranksdead.id@gmail.com>
2022-03-23 03:51:32 +0700

    helperurl

--- lib/helperUrl.dart
+++ lib/helperUrl.dart
@@ -1,5 +1,6 @@
 const String url_test = "http://10.0.2.2:3000/users/authenticate";
-const String login_rest = "http://10.0.2.2:3000/users/authenticate";
+const String login_rest = "http://10.0.2.2:4000/users/authenticate";
+const String get_profile_byusername = "http://10.0.2.2:4000/users/username/";
 
 class MyUrl {
   // String urlDevice = "http://192.168.0.104/school-management/";
